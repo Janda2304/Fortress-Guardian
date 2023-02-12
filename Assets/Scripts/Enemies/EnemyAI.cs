10fcd71cddf4084e25736367dd9bd9034491d502
Janda <ondra.janous@email.cz>
2023-02-12 14:45:10 +0100

    towerAI, improved enemyAI, tips&tricks, sound effects & much more

--- Assets/Scripts/Enemies/EnemyAI.cs
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -1,30 +1,45 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.AI;
+using FG_BuildingSystem;
 
 public class EnemyAI : MonoBehaviour
 {
+    #region Variables
+    [Header("Patrol Settings")]
     [SerializeField] private NavMeshAgent agent;
     public Vector3 spawnpoint = new Vector3();
     [SerializeField] private Vector3 destination = new Vector3();
+    [Header("Animations")]
     public Animator animator;
+    [Header("Other Scripts")]
+    [SerializeField] private Currency _currency;
+
+    [Header("Enemy Stats Settings")] 
+    public float maxHealth = 100f;
+    [HideInInspector] public float health;
+    [SerializeField] private float damage = 0.5f;
+    [SerializeField] private int moneyGain = 25;
+
+    
+
+    
     private bool isAttacking;
     private Collider[] hitColliders;
-    //TODO: Dealing double the damage when only one enemy is attacking
-    private float damage = 0.5f;
-    //
+    #endregion
 
     void Start()
     {
         #region Setup
 
         spawnpoint = GameObject.FindGameObjectWithTag("Spawnpoint").transform.position;
+        health = maxHealth;
         transform.position = spawnpoint;
         animator.SetBool("Run", true);
         destination = GameObject.FindGameObjectWithTag("Castle").transform.position;
         agent.SetDestination(destination);
+        _currency = GameObject.FindGameObjectWithTag("GameController").GetComponent<Currency>();
         #endregion Setup
 
     }
@@ -41,21 +56,42 @@ public class EnemyAI : MonoBehaviour
             animator.SetBool("Run", false);
             animator.SetBool("Attack", true);
             agent.isStopped = true;
+            agent.destination = hitColliders[0].transform.position;
         }
-        if (!isAttacking)
+        if (!isAttacking &&)
         {
             animator.SetBool("Run", true);
             animator.SetBool("Attack", false);
             agent.isStopped = false;
-            
+            agent.destination = destination;
 
 
 
+
+        }
+
+       
+        
+        if (Vector3.Distance(transform.position, destination) < 6f && !isAttacking)
+        {
+            agent.isStopped = true;
+            animator.SetBool("Run", false);
+            animator.SetBool("Idle", true);
+            agent.isStopped = true;
         }
            
         
         
 
+    }
+
+    private void FixedUpdate()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            _currency.coins += moneyGain;
+        }
     }
 
     void DealDamage()
@@ -66,12 +102,23 @@ public class EnemyAI : MonoBehaviour
             {
                 if (hitCollider.gameObject.CompareTag("Building"))
                 {
-                    hitCollider.GetComponent<BuildingBehaviour>().TakeDamage(damage);
+                    hitCollider.GetComponentInParent<BuildingBehaviour>().TakeDamage(damage);
+                   
+                    transform.LookAt(hitCollider.transform);
+                }
+                else if (hitCollider.gameObject.CompareTag("Castle"))
+                {
+                    _currency.health -= damage;
                 }
               
             }
         }
        
+    }
+    
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
     }
 
 }
